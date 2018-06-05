8f6961eba181a30f1e5a8a917eedb9ba3b2fb5d1
randhyllcho <randhyllcho@gmail.com>
2018-06-04 23:23:41 -0700

    updated date ranges and changed how the data_frame was built after looping.

--- baseballScraper.R
+++ baseballScraper.R
@@ -1,9 +1,10 @@
 library(tidyverse)
 library(baseballr)
+library(BRRR)
 library(lubridate)
 
-start_dates <- as.character(seq(ymd("2018-03-01"),ymd("2018-04-18"), by = "weeks"))
-end_dates <- as.character(seq(ymd("2018-03-07"),ymd("2018-04-23"), by = "weeks"))
+start_dates <- as.character(seq(ymd("2018-03-01"), ymd(Sys.Date() - 6), by = "weeks"))
+end_dates <- as.character(seq(ymd("2018-03-07"), ymd(Sys.Date()), by = "weeks"))
 
 game_dates <- tibble(start_dates, end_dates)
 
@@ -15,18 +16,14 @@ rows <- list()
 pitcher_rows <- list()
 
 for (i in seq(1,nrow(game_dates))) {
-  dat <- scrape_statcast_savant_batter_all(start_dates[i], end_dates[i])
-  rows[[i]] <- dat
-}
-
-for (i in seq(1,nrow(game_dates))) {
   dat <- scrape_statcast_savant_pitcher_all(start_dates[i], end_dates[i])
   pitcher_rows[[i]] <- dat
-}
+}; skrrrahh(43)
 
 hitter_data <- rbind_list(rows)
-pitcher_data <- rbind_list(pitcher_rows)
+pitcher_data <- do.call(rbind, pitcher_rows)
+
 
 write.csv(hitter_data, "marapr2018_Hitters.csv", row.names = FALSE)
-write.csv(pitcher_data, "marapr2018_Pitchers.csv", row.names = FALSE)
+write.csv(pitcher_data, "season2018_Pitchers.csv", row.names = FALSE)
 
