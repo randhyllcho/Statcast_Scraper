1b88fe95fbd080a1e10650d07fbc4bc18ac33a66
randhyllcho <randhyllcho@gmail.com>
2018-09-14 19:49:08 -0700

    Updated script to push data into MySQL DB

--- baseballScraper.R
+++ baseballScraper.R
@@ -1,28 +1,39 @@
-library(tidyverse)
-library(baseballr)
-library(BRRR)
-library(lubridate)
+pacman::p_load(pool, DBI, tidyverse, keyringr, RMySQL, baseballr, BRRR, lubridate)
 
-start_dates <- as.character(seq(ymd("2018-03-01"), ymd(Sys.Date() - 6), by = "weeks"))
-end_dates <- as.character(seq(ymd("2018-03-07"), ymd(Sys.Date()), by = "weeks"))
+db <- dbPool(MySQL(),
+             dbname = 'Pitchers_2018',
+             host = 'localhost',
+             username = 'root',
+             password = decrypt_kc_pw('pitchers_DB'))
 
-game_dates <- tibble(start_dates, end_dates)
+beg_date <- as.character(ymd(Sys.Date() - 6))
+end_date <- as.character(ymd(Sys.Date()))
 
-game_dates <- game_dates %>% 
-  filter(!month(start_dates) %in% c(11,12,1)) %>% 
-  mutate_at(c("start_dates", "end_dates"), as.character)
+if (db_has_table(db, "Pitchers")) {
+  pitcher_data <- scrape_statcast_savant_pitcher_all(beg_date, end_date); skrrrahh(43)
+  print("New Data writing to table")
+} else {
+  start_dates <- as.character(seq(ymd("2018-03-01"), beg_date, by = "weeks"))
+  end_dates <- as.character(seq(ymd("2018-03-07"), end_date, by = "weeks"))
+  
+  game_dates <- tibble(start_dates, end_dates)
+  
+  game_dates <- game_dates %>% 
+    filter(!month(start_dates) %in% c(11,12,1)) %>% 
+    mutate_at(c("start_dates", "end_dates"), as.character)
+  
+  pitcher_rows <- list()
+  
+  for (i in seq(1,nrow(game_dates))) {
+    dat <- scrape_statcast_savant_pitcher_all(start_dates[i], end_dates[i])
+    pitcher_rows[[i]] <- dat
+    Sys.sleep(30)
+  }; skrrrahh(43)
+  
+  pitcher_data <- do.call(rbind, pitcher_rows)
+  print("All the Data writing to table")
+}
 
-rows <- list()
-pitcher_rows <- list()
+dbWriteTable(db, "Pitchers", pitcher_data, append = TRUE)
 
-for (i in seq(1,nrow(game_dates))) {
-  dat <- scrape_statcast_savant_pitcher_all(start_dates[i], end_dates[i])
-  pitcher_rows[[i]] <- dat
-}; skrrrahh(43)
-
-#hitter_data <- rbind_list(rows)
-pitcher_data <- do.call(rbind, pitcher_rows)
-
-#write.csv(hitter_data, "marapr2018_Hitters.csv", row.names = FALSE)
-write.csv(pitcher_data, "season2018_Pitchers.csv", row.names = FALSE)
-
+poolClose(db)
