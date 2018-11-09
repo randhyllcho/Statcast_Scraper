b2dbeaf223909d3da624d61cded3cec585012071
RC <thatdevrc@gmail.com>
2018-11-08 16:23:27 -0800

    Fixed scope issues that prevented script from running.

--- baseballScraper.R
+++ baseballScraper.R
@@ -9,30 +9,25 @@ db <- dbPool(MySQL(),
 beg_date <- as.character(ymd(Sys.Date() - 6))
 end_date <- as.character(ymd(Sys.Date()))
 
-if (db_has_table(db, "Pitchers")) {
-  pitcher_data <- scrape_statcast_savant_pitcher_all(beg_date, end_date); skrrrahh(43)
-  print("New Data writing to table")
-} else {
-  start_dates <- as.character(seq(ymd("2018-03-01"), beg_date, by = "weeks"))
-  end_dates <- as.character(seq(ymd("2018-03-07"), end_date, by = "weeks"))
+start_dates <- as.character(seq(ymd("2018-03-01"), beg_date, by = "weeks"))
+end_dates <- as.character(seq(ymd("2018-03-07"), end_date, by = "weeks"))
+
+game_dates <- tibble(start_dates, end_dates)
+
+game_dates <- game_dates %>% 
+  filter(!month(start_dates) %in% c(11,12,1)) %>% 
+  mutate_at(c("start_dates", "end_dates"), as.character)
+
+pitcher_rows <- list()
+
+for (i in seq(1,nrow(game_dates))) {
+  dat <- scrape_statcast_savant_pitcher_all(start_dates[i], end_dates[i])
+  pitcher_rows[[i]] <- dat
+  Sys.sleep(30)
+}; skrrrahh(43)
   
-  game_dates <- tibble(start_dates, end_dates)
-  
-  game_dates <- game_dates %>% 
-    filter(!month(start_dates) %in% c(11,12,1)) %>% 
-    mutate_at(c("start_dates", "end_dates"), as.character)
-  
-  pitcher_rows <- list()
-  
-  for (i in seq(1,nrow(game_dates))) {
-    dat <- scrape_statcast_savant_pitcher_all(start_dates[i], end_dates[i])
-    pitcher_rows[[i]] <- dat
-    Sys.sleep(30)
-  }; skrrrahh(43)
-  
-  pitcher_data <- do.call(rbind, pitcher_rows)
-  print("All the Data writing to table")
-}
+pitcher_data <- do.call(rbind, pitcher_rows)
+print("All the Data writing to table")
 
 dbWriteTable(db, "Pitchers", pitcher_data, append = TRUE)
 
