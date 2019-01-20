bd78872dfb197fd6d07f65b5aa6f1b9a4fa89dcd
RC <thatdevrc@gmail.com>
2019-01-20 02:27:05 -0800

    Updated to use Postgresql. Fixed date issues.

--- baseballScraper.R
+++ baseballScraper.R
@@ -1,19 +1,17 @@
-pacman::p_load(pool, DBI, tidyverse, keyringr, RMySQL, baseballr, BRRR, lubridate)
+pacman::p_load(pool, DBI, tidyverse, keyringr, RPostgreSQL, baseballr, BRRR, lubridate)
 
-db <- dbPool(MySQL(),
-             dbname = 'Pitchers_2018',
-             host = 'localhost',
-             username = 'root',
-             password = decrypt_kc_pw('pitchers_DB'))
+db <- dbPool(PostgreSQL(),
+             dbname = 'pitchers_2018',
+             host = 'localhost')
 
-beg_date <- as.character(ymd(Sys.Date() - 6))
-end_date <- as.character(ymd(Sys.Date()))
+beg_date <- ymd(Sys.Date() - 6)
+end_date <- ymd(Sys.Date())
 
 start_dates <- as.character(seq(ymd("2018-03-01"), beg_date, by = "weeks"))
 end_dates <- as.character(seq(ymd("2018-03-07"), end_date, by = "weeks"))
 
 game_dates <- tibble(start_dates, end_dates)
-
+month(game_dates$start_dates)
 game_dates <- game_dates %>% 
   filter(!month(start_dates) %in% c(11,12,1)) %>% 
   mutate_at(c("start_dates", "end_dates"), as.character)
@@ -23,12 +21,13 @@ pitcher_rows <- list()
 for (i in seq(1,nrow(game_dates))) {
   dat <- scrape_statcast_savant_pitcher_all(start_dates[i], end_dates[i])
   pitcher_rows[[i]] <- dat
-  Sys.sleep(30)
+  Sys.sleep(10)
 }; skrrrahh(43)
   
 pitcher_data <- do.call(rbind, pitcher_rows)
-print("All the Data writing to table")
 
-dbWriteTable(db, "Pitchers", pitcher_data, append = TRUE)
+glimpse(pitcher_data)
+
+dbWriteTable(db, "Pitchers", pitcher_data)
 
 poolClose(db)
